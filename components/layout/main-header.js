4020ba1bf1904bf13749498ba083d69e5f3c9de7
Gabor Csecsetka <csecsi85@gmail.com>
2021-05-11 22:33:39 +0100

    404 page and layout wrapper added

---
+++ components/layout/main-header.js
@@ -0,0 +1,16 @@
+import classes from './main-header.module.css'
+
+function MainHeader() {
+  return (
+    <header>
+      <div>
+        this is where the logo will go using next Link
+      </div>
+      <nav>
+        navigation will go here using next Link
+      </nav>
+    </header>
+  )
+}
+
+export default MainHeader;

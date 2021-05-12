093aaf4fdaf4f89872e53a3635b34acf391469a1
Gabor Csecsetka <csecsi85@gmail.com>
2021-05-12 22:34:04 +0100

    Finished first navigation version

--- components/layout/MainHeader.js
+++ components/layout/MainHeader.js
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import Image from 'next/image';
 import Link from 'next/link';
 import classes from '../../styles/components/MainHeader.module.scss';
 import { useRouter } from 'next/router';
@@ -10,6 +9,25 @@ import { FaUserCircle } from 'react-icons/fa';
 function MainHeader() {
   const [isOpened, setIsOpened] = useState(false);
 
+  const menuItems = [
+    {
+      url: '/',
+      name: 'Home'
+    },
+    {
+      url: '/about',
+      name: 'About'
+    },
+    {
+      url: '/docs',
+      name: 'Docs'
+    },
+    {
+      url: '/contact',
+      name: 'Contact'
+    }
+  ];
+
   const toggleModal = () => {
     setIsOpened(!isOpened);
   };
@@ -19,43 +37,31 @@ function MainHeader() {
   const router = useRouter();
   return (
     <header className={classes.main}>
-      <LogoIcon
-        linkPath={'/'}
-        logoPath={'/bamboos-logo-cropped.jpeg'}
-        altText={'Bamboos logo'}
-        width={35}
-        height={35}
-      />
+      <div className={classes.logoWrapper}>
+        <LogoIcon
+          linkPath={'/'}
+          logoPath={'/bamboos-logo-cropped.jpeg'}
+          altText={'Bamboos logo'}
+          width={35}
+          height={35}
+        />
+        <Link href={'/'}>
+          <h1>Project Zone</h1>
+        </Link>
+      </div>
       <nav>
         <ul className={classes.list}>
-          <li
-            className={`${router.pathname === '/' ? classes.active : ''} ${
-              classes.listItem
-            }`}
-          >
-            <Link href="/">Home</Link>
-          </li>
-          <li
-            className={`${router.pathname === '/about' ? classes.active : ''} ${
-              classes.listItem
-            }`}
-          >
-            <Link href="/about">About</Link>
-          </li>
-          <li
-            className={`${router.pathname === '/docs' ? classes.active : ''} ${
-              classes.listItem
-            }`}
-          >
-            <Link href="/docs">Docs</Link>
-          </li>
-          <li
-            className={`${
-              router.pathname === '/contact' ? classes.active : ''
-            } ${classes.listItem}`}
-          >
-            <Link href="/contact">Contact</Link>
-          </li>
+          {menuItems.map(item => {
+            return (
+              <li
+                className={`${
+                  router.pathname === item.url ? classes.active : ''
+                } ${classes.listItem}`}
+              >
+                <Link href={item.url}>{item.name}</Link>
+              </li>
+            );
+          })}
           <li className={classes.listItem} onClick={toggleModal}>
             <a>
               <FaUserCircle />

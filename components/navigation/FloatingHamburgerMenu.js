05ee158ab1af368f46d75f3bd8bd5351f1c7eeba
Gabor Csecsetka <csecsi85@gmail.com>
2021-05-13 11:47:36 +0100

    Modal closing icon added and transition for button

--- components/navigation/FloatingHamburgerMenu.js
+++ components/navigation/FloatingHamburgerMenu.js
@@ -1,14 +1,31 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 
 import classes from '../../styles/components/FloatingHamburgerMenu.module.scss';
 import MenuModal from '../modals/MenuModal';
 
 const FloatingHamburgerMenu = () => {
+  const [isOpened, setIsOpened] = useState(false);
+
+  const toggleModal = () => {
+    setIsOpened(!isOpened);
+  };
+
   return (
     <Fragment>
-      <MenuModal />
-      <div className={classes.hamburgerWrapper}>
-        <span className={classes.hamburgerIcon}>&nbsp;</span>
+      {isOpened ? <MenuModal /> : null}
+      <div
+        className={
+          !isOpened
+            ? `${classes.hamburgerWrapper} ${classes.openWrapper}`
+            : `${classes.hamburgerWrapper} ${classes.closeWrapper}`
+        }
+        onClick={toggleModal}
+      >
+        {
+          <span
+            className={!isOpened ? classes.hamburgerIcon : classes.closeIcon}
+          />
+        }
       </div>
     </Fragment>
   );

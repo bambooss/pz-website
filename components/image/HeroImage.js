402ac2c70740cfe437e5268109447d14843fc53b
Gabor Csecsetka <csecsi85@gmail.com>
2021-07-04 23:06:57 +0100

    Started hero image container text

--- components/image/HeroImage.js
+++ components/image/HeroImage.js
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import LogoIcon from '../navigation/LogoIcon';
 import classes from '../../styles/components/HeroImage.module.scss';
 
 const HeroImage = ({ children }) => (
@@ -12,6 +13,19 @@ const HeroImage = ({ children }) => (
         objectFit="cover"
         priority="true"
       />
+    </div>
+    <div className={classes['hero-content-container']}>
+      <LogoIcon className={classes.logo} width={70} height={70} />
+      <h1>CoPro</h1>
+      <h2>
+        <i>Where project ideas come to life</i>
+      </h2>
+      <p>
+        We know how difficult is for new programmers to find projects to work
+        on. We have been there. This is the reason why we have created this
+        application, so you can easily find projects that suit your stack and
+        skill level.
+      </p>
     </div>
     <div className={classes['hero-filler']} />
   </>
